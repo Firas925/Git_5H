31700ab1331229e26fa7fa59a714ac332f280174
Firas925 <62358667+Firas925@users.noreply.github.com>
2020-11-29 16:44:41 +0100

    Update Monstre.java

--- 5H_Game/src/win/main/Monstre.java
+++ 5H_Game/src/win/main/Monstre.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package win.main;
 
 import java.awt.Color;
@@ -30,7 +29,7 @@ public class Monstre  extends GameObject {
 		for(int i = 0 ;i<handler.object.size();i++ ) {
 			
 			  GameObject tempObject =handler.object.get(i) ;
-			  if(tempObject.getId()==ID.boîte ) {
+			  if(tempObject.getId()==ID.boÃ®te ) {
 				  
 				  if(getBoundsBig().intersects(tempObject.getBounds())) {
 					  VelX+= VelX*2 -1 ;
@@ -74,80 +73,4 @@ public class Monstre  extends GameObject {
 	
 
 }
-=======
-package win.main;
 
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Rectangle;
-import java.util.Random;
-
-public class Monstre  extends GameObject {
-	
-	private Handler handler ;
-	
-	Random r = new Random() ;
-	int choose = 0 ; 
-	int hp=100 ;
-	
-
-	public Monstre(int x, int y, ID id , Handler handler) {
-		super(x, y, id);
-		this.handler=handler ;
-	}
-
-	
-	public void tick() {
-		x+=VelX;
-		y+=VelY ;
-		
-		choose = r.nextInt(5) ;
-		for(int i = 0 ;i<handler.object.size();i++ ) {
-			
-			  GameObject tempObject =handler.object.get(i) ;
-			  if(tempObject.getId()==ID.boîte ) {
-				  
-				  if(getBoundsBig().intersects(tempObject.getBounds())) {
-					  VelX+= VelX*2 -1 ;
-					  VelY+= VelY*2 -1 ; 
-					  VelX *= -0.60;
-					  VelY *= -0.60 ;
-					  
-				  } else if (choose ==0) {
-						VelX=(r.nextInt(4 - -4)+ -4) ;
-						VelY=(r.nextInt(4 - -4)+ -4) ;
-					  
-			  }
-		 } 
-	
-		}
-	}
-
-	
-	public void render(Graphics g) {
-		g.setColor(Color.red) ;
-		g.fillRect(x, y, 32, 32);
-		
-		
-		
-		 
-		
-		
-	}
-
-	
-	public Rectangle getBounds() {
-		
-		return new Rectangle(x,y,32,32);
-	}
-	
-	
-	public Rectangle getBoundsBig() {
-		
-		return new Rectangle(x-20,y-20,64,64);
-	} 
-	
-
-}
->>>>>>> 834a61652a505f5642eef153b27254b0c0bfbebb
