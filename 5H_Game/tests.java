56c6819c20882f790ba5af2a44cbe01775c6a7fe
Flora-hub <74180816+Flora-hub@users.noreply.github.com>
2021-01-15 10:01:21 +0100

    Add files via upload

--- 5H_Game/tests.java
+++ 5H_Game/tests.java
@@ -2,12 +2,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
 
 import win.main.*; 
 
 import org.junit.jupiter.api.Test;
 
-class testSprint3 {
+class tests {
 
 	@Test
 	void testHerosDown() {
@@ -16,8 +17,11 @@ class testSprint3 {
 			Handler handler = new Handler();
 			int x = 50;
 			int y = 50;
-			hero Heros = new hero(x,y,ID.joueur,handler, game);
-			
+			Camera camera = new Camera(x,y);
+			BufferedImageLoader loader  = new BufferedImageLoader();
+			BufferedImage image = loader.loadImage("/wizard_images.png");
+			SpriteSheet ss = new SpriteSheet(image);
+			hero Heros = new hero(x,y,ID.joueur,handler, game,camera,ss);
 			handler.setDown(true);
 			Heros.tick();
 			handler.setDown(false);
@@ -26,16 +30,20 @@ class testSprint3 {
 		}
 	
 
-	// le héros ne traverse pas les murs
+	// le hÃ©ros ne traverse pas les murs
 	@SuppressWarnings("unused")
 	@Test
 	public void testMurHeros() {
 
 		Game game = new Game(); 
 		Handler handler = new Handler();
-		hero heros = new hero(50,50, ID.joueur ,handler, game);
+		Camera camera = new Camera(50,50);
+		BufferedImageLoader loader  = new BufferedImageLoader();
+		BufferedImage image = loader.loadImage("/wizard_images.png");
+		SpriteSheet ss = new SpriteSheet(image);
+		hero heros = new hero(50,50, ID.joueur ,handler, game,camera,ss);
 		int y = heros.getY();
-		Box box = new Box(50,53,ID.boîte);
+		Box box = new Box(50,53,ID.boîte,ss);
 		handler.setUp(true);
 		heros.tick();
 		assertEquals(y,heros.getY());
@@ -48,23 +56,29 @@ class testSprint3 {
 
 		Game game = new Game(); 
 		Handler handler = new Handler();
-		Monstre Monstre = new Monstre(50,50, ID.monstre ,handler, game);
+		BufferedImageLoader loader  = new BufferedImageLoader();
+		BufferedImage image = loader.loadImage("/wizard_images.png");
+		SpriteSheet ss = new SpriteSheet(image);
+		Monstre Monstre = new Monstre(50,50, ID.monstre ,handler, game,ss);
 		int y = Monstre.getY();
-		Box box = new Box(50,53,ID.boîte);
+		Box box = new Box(50,53,ID.boîte,ss);
 		handler.setUp(true);
 		Monstre.tick();
 		assertEquals(y,Monstre.getY());
 	}
 	
-	// les fantômes traversent les murs
+	// les fantÃ´mes traversent les murs
 	@Test
 	public void testMurFantome() {
 
 		Game game = new Game(); 
 		Handler handler = new Handler();
-		Fantome fantome = new Fantome(50,50, ID.fantome ,handler, game);
+		BufferedImageLoader loader  = new BufferedImageLoader();
+		BufferedImage image = loader.loadImage("/wizard_images.png");
+		SpriteSheet ss = new SpriteSheet(image);
+		Fantome fantome = new Fantome(50,50, ID.fantome ,handler, game,ss);
 		int y = fantome.getY();
-		Box box = new Box(50,53,ID.boîte);
+		Box box = new Box(50,53,ID.boîte,ss);
 		while(fantome.getY()<=box.getY()) {
 			handler.setUp(true);
 			fantome.tick();
@@ -74,15 +88,19 @@ class testSprint3 {
 	}
 	
 	
-	// le héros perd des points de vie lorsqu'il marche sur une case piège
+	// le hÃ©ros perd des points de vie lorsqu'il marche sur une case piÃ¨ge
 	
 	@Test
 	public void testPertePVPiege() {
 		Game game = new Game(); 
 		Handler handler = new Handler();
-		hero Heros = new hero(80,80,ID.joueur,handler,game);
+		Camera camera = new Camera(80,80);
+		BufferedImageLoader loader  = new BufferedImageLoader();
+		BufferedImage image = loader.loadImage("/wizard_images.png");
+		SpriteSheet ss = new SpriteSheet(image);
+		hero Heros = new hero(80,80,ID.joueur,handler,game,camera,ss);
 		int pv = 100; 
-		magique Magique = new magique(46, 72, ID.magique);
+		magique Magique = new magique(46, 72, ID.magique,ss);
 		while (Heros.getX()>16) {
 			handler.setLeft(true);
 			Heros.tick();
@@ -91,14 +109,18 @@ class testSprint3 {
 		assertTrue(win.main.Game.ptvieH<pv);
 	}
 	
-	// le héros gagne des points de vie lorsqu'il marche sur une case magique
+	// le hÃ©ros gagne des points de vie lorsqu'il marche sur une case magique
 	@Test
 	public void testGainPVMagique() {
 		Game game = new Game(); 
 		Handler handler = new Handler();
-		hero Heros = new hero(80,80,ID.joueur,handler,game);
+		Camera camera = new Camera(80,80);
+		BufferedImageLoader loader  = new BufferedImageLoader();
+		BufferedImage image = loader.loadImage("/wizard_images.png");
+		SpriteSheet ss = new SpriteSheet(image);
+		hero Heros = new hero(80,80,ID.joueur,handler,game,camera,ss);
 		int pv = 100; 
-		magique Magique = new magique(46, 72, ID.magique);
+		magique Magique = new magique(46, 72, ID.magique,ss);
 		while (Heros.getX()>16) {
 			handler.setLeft(true);
 			Heros.tick();
@@ -107,26 +129,23 @@ class testSprint3 {
 		assertTrue(win.main.Game.ptvieH>pv);
 	}
 	
-	// le monstre perd des points de vie lorsque le héros l'attaque
+	// le monstre perd des points de vie lorsque le hÃ©ros l'attaque
 	@Test
 	public void testPertePVMonstre() {
 		Game game = new Game(); 
 		Handler handler = new Handler();
+		BufferedImageLoader loader  = new BufferedImageLoader();
+		BufferedImage image = loader.loadImage("/wizard_images.png");
+		SpriteSheet ss = new SpriteSheet(image);
 		
-		Monstre Monstre = new Monstre(50,50, ID.monstre ,handler, game);
+		Monstre Monstre = new Monstre(50,50, ID.monstre ,handler, game,ss);
 		int pv = Game.ptvieM; 
 		int balle = Game.balle;
 		Monstre.tick();
 		assertTrue(Game.ptvieM<pv);
-		// trop compliqué à tester car pour attaquer, le joueur doit cliquer sur la souris et les balles ont une trajectoire trop aléatoire
+		// trop compliquÃ© Ã  tester car pour attaquer, le joueur doit cliquer sur la souris et les balles ont une trajectoire trop alÃ©atoire
 		
 	}
 }
 	
-	
-
-
-
-
-
 	
